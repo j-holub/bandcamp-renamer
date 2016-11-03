36a9c37376a727b9e6c03969f6728efdefc3ae60
Jan Holub <Jan-Holub@gmx.net>
2016-11-03 20:15:40 +0100

    Added function to parse the song information

--- bc_renamer.py
+++ bc_renamer.py
@@ -1,6 +1,36 @@
 import argparse
 import os
+import re
 import sys
+
+# ######### #
+# Functions #
+# ######### #
+
+
+# Parses a filename from bandcamp and extracts the information about the artist, album, tracknumber
+# and title
+#
+# @param filename
+#
+# @return object containing the infos
+# 'artist', 'album', 'tracknumber', 'songname'
+def parseInfoFromSong(song):
+	# get the arist-, album- and songname
+	artist, album, songname = str.split(song, ' - ')
+	# seperate track number from track title
+	tracknumber = songname[0:2]
+	songname = songname[2:]
+
+	# return object
+	songinfo = {
+		'artist': artist,
+		'album': album,
+		'songname': songname,
+		'tracknumber': tracknumber
+	}
+
+	return songinfo
 
 
 # ################ #
