dc8a5d7123e5f20f2c2ecbb4e99c8b02d0daee67
Jan Holub <Jan-Holub@gmx.net>
2016-11-08 16:26:20 +0100

    Added a function to rename a single file

--- bc_renamer.py
+++ bc_renamer.py
@@ -11,7 +11,7 @@ import sys
 # Parses a filename from bandcamp and extracts the information about the artist, album, tracknumber
 # and title
 #
-# @param filename
+# @param song - filename (without the extension)
 #
 # @return object containing the infos
 # 'artist', 'album', 'tracknumber', 'songname'
@@ -32,6 +32,40 @@ def parseInfoFromSong(song):
 
 	return songinfo
 
+# --------------------------
+
+# Takes the absolute path to a file and renames it according to the format given
+#
+# @param path   - absolute path to the songfile
+# @param format - format that the file should be renamed to
+# 
+# @return void 
+def renameSongFile(path, format):
+
+	# split the path
+	dir = os.path.dirname(path)
+	filename, extension  = os.path.splitext(os.path.basename(path))
+
+	# get the songinfo
+	info = parseInfoFromSong(filename)
+
+	# apply the songinfo to the format string
+	new_songname = format
+
+	# artist
+	new_songname = str.replace(new_songname, "%a", info['artist'])
+	# album
+	new_songname = str.replace(new_songname, "%A", info['album'])
+	# songname
+	new_songname = str.replace(new_songname, "%t", info['songname'])
+	# track number
+	new_songname = str.replace(new_songname, "%n", info['tracknumber'])
+
+	# rename the file
+	os.rename(path, "%s/%s%s" % (dir, new_songname, extension))
+
+# -------------------------------
+
 
 # ################ #
 # Argument Parsing #
@@ -50,3 +84,27 @@ parser = argparse.ArgumentParser(description="Renames songs downloaded from band
 parser.add_argument('path', help='Path to the directory containing the files or the file itself')
 parser.add_argument('format', help=formatHelp)
 arguments = parser.parse_args()
+
+
+# ##################### #
+# Argument verification #
+# ##################### #
+
+# get the absolute path
+path = os.path.abspath(arguments.path)
+
+# check if path is actually a path or a file
+if(not os.path.isdir(path) and not os.path.isfile(path)):
+	print "ERROR: '%s' is neither a file nor a directory" % path
+	sys.exit()
+
+# check if it is a full directory that has to be renamed
+if(os.path.isdir(path)):
+	# TODO rename folder
+	print ("dir")
+
+# check if it's only one file
+elif(os.path.isfile(path)):
+	# TODO rename file
+	#print("file")
+	renameSongFile(path, arguments.format)
